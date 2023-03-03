55bffb292b633901f8ffb563f53dc397227f44cf
Tim <timdereuse@gmail.com>
2023-03-02 20:52:35 -0500

    init from old projet

---
+++ audio_slice.py
@@ -0,0 +1,32 @@
+import feature_extraction as fe
+from importlib import reload
+from librosa import resample
+from librosa.util.exceptions import ParameterError
+import numpy as np
+reload(fe)
+
+class AudioSlice(object):
+
+    def __init__(self, audio, sr, fname=None, normalize=True):
+
+        try:
+            self.feats = fe.extract_features_from_audio(audio, sr)
+        except ParameterError:
+            self.feats = None
+            return
+
+        self.fname = fname
+        self.audio = audio
+        if normalize:
+            self.audio = audio / max(np.abs(audio))
+        self.sr = sr
+
+    def get_rev_slice(self):
+        new_slice = AudioSlice(self.audio[::-1], self.sr, self.fname)
+        return new_slice
+
+    def get_resampled_slice(self, scale_amt=None):
+        if not scale_amt:
+            scale_amt = np.random.exponential(0.9)
+        new_slice = AudioSlice(resample(self.audio[::-1], self.sr, self.sr * scale_amt), self.sr, self.fname)
+        return new_slice
---
+++ feature_extraction.py
@@ -0,0 +1,40 @@
+import librosa as rosa
+import numpy as np
+
+
+def extract_features_from_audio(inp, sr, len_segments=2**11, n_segments=16, normalize=True):
+    n_fft = len_segments * 2
+    hop = len_segments // 2
+
+    # normalize sound before feature extraction
+    if max(inp) > 0 and normalize:
+        inp = np.clip(inp / max(inp), 0, 1)
+
+    # extend with 0s if we are trying to get more feature frames than this is long
+    if len(inp) < len_segments * n_segments:
+        extend = (len_segments * n_segments) - len(inp)
+        inp = np.concatenate([inp, np.zeros(extend)])
+
+    # comment out lines to exclude items from the featureset
+    feats = {
+        'spec_centroid': rosa.feature.spectral_centroid(inp, sr, n_fft=n_fft, hop_length=hop),
+         # 'spec_contrast': rosa.feature.spectral_contrast(inp, sr),
+         # 'spec_bandwidth': rosa.feature.spectral_bandwidth(inp, sr, n_fft=n_fft, hop_length=hop),
+         # 'spec_flatness': rosa.feature.spectral_flatness(inp, n_fft=n_fft, hop_length=hop),
+         # 'spec_rolloff': rosa.feature.spectral_rolloff(inp, sr, n_fft=n_fft, hop_length=hop),
+        'rms': rosa.feature.rms(inp, frame_length=n_fft, hop_length=hop),
+    }
+
+    # feats['zero_crossing'] = rosa.feature.zero_crossing_rate(inp, frame_length=n_fft, hop_length=hop)
+
+    flat_feats = {}
+    for key in feats.keys():
+        feat_seq = feats[key].ravel()
+
+        for i in range(n_segments):
+            try:
+                flat_feats[f'{key}_{i}'] = feat_seq[i]
+            except IndexError:
+                flat_feats[f'{key}_{i}'] = 0
+
+    return flat_feats
